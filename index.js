58322aab1fe8cc03b7cd4fa9d0cdeb8a3cfb1fb5
Labib90 <labib724@gmail.com>
2022-06-04 18:01:47 +0700

    #

--- index.js
+++ index.js
@@ -6,15 +6,28 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (arrData, fnSort) => {
+  let nameSort = fnSort(arrData);
+  let arrNewName = [];
+  for (let index = 0; index < nameSort.length; index++) {
+    const element = `${index + 1}. ${nameSort[index]}`;
+    arrNewName.push(element);
+  }
+  return arrNewName;
+}
+
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (arrData) => {
+  return arrData.sort();
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (names){
+  return names.reverse()
+}
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
